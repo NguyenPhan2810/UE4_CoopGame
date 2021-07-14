3a76ae79197b221da02fd27d29deb1a60897cac2
Phan Quý Nguyên <pqn2810@gmail.com>
2021-07-14 10:24:50 +0700

    Add tracker bot

--- Source/LearnUE4_CoopGame/LearnUE4_CoopGameGameModeBase.cpp
+++ Source/LearnUE4_CoopGame/LearnUE4_CoopGameGameModeBase.cpp
@@ -6,7 +6,7 @@
 ALearnUE4_CoopGameGameModeBase::ALearnUE4_CoopGameGameModeBase()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_SPlayer"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_PlayerPawn"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
---
+++ Source/LearnUE4_CoopGame/Private/AI/STrackerBot.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/STrackerBot.h"
+#include <Components/StaticMeshComponent.h>
+
+
+// Sets default values
+ASTrackerBot::ASTrackerBot()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	MeshComponent->SetCanEverAffectNavigation(false);
+}
+
+// Called when the game starts or when spawned
+void ASTrackerBot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASTrackerBot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
