4a93e303957ca391ef2c6892bd2a182ac75b473a
Phan Quý Nguyên <pqn2810@gmail.com>
2021-07-26 11:04:40 +0700

    Add new SGameMode

--- Source/LearnUE4_CoopGame/Private/SGameMode.cpp
+++ Source/LearnUE4_CoopGame/Private/SGameMode.cpp
@@ -1,9 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "LearnUE4_CoopGameGameModeBase.h"
+#include "SGameMode.h"
 
-ALearnUE4_CoopGameGameModeBase::ALearnUE4_CoopGameGameModeBase()
+ASGameMode::ASGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_PlayerPawn"));
---
+++ Source/LearnUE4_CoopGame/Public/SGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNUE4_COOPGAME_API ASGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ASGameMode();
+};
